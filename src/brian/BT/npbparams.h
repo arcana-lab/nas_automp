d47cc6c88dbe7203a74a1a94998eb9e2b5fac185
Brian Daniel Homerding <bdh2903@peroni.cs.northwestern.edu>
2021-03-28 21:25:42 -0500

    Update README and change src for BT, FT to class B

--- src/brian/BT/npbparams.h
+++ src/brian/BT/npbparams.h
@@ -1,14 +1,14 @@
-/* CLASS = C */
+/* CLASS = B */
 /*
 c  This file is generated automatically by the setparams utility.
 c  It sets the number of processors and the class of the NPB
 c  in this directory. Do not modify it by hand.
 */
-#define	PROBLEM_SIZE	162
+#define	PROBLEM_SIZE	102
 #define	NITER_DEFAULT	200
-#define	DT_DEFAULT	0.0001
+#define	DT_DEFAULT	0.0003
 #define	CONVERTDOUBLE	FALSE
-#define COMPILETIME "25 Mar 2021"
+#define COMPILETIME "28 Mar 2021"
 #define NPBVERSION "3.0 structured"
 #define CS1 "gclang"
 #define CS2 "gclang"
--- src/brian/FT/npbparams.h
+++ src/brian/FT/npbparams.h
@@ -1,17 +1,17 @@
-/* CLASS = C */
+/* CLASS = B */
 /*
 c  This file is generated automatically by the setparams utility.
 c  It sets the number of processors and the class of the NPB
 c  in this directory. Do not modify it by hand.
 */
 #define	NX	512
-#define	NY	512
-#define	NZ	512
+#define	NY	256
+#define	NZ	256
 #define	MAXDIM	512
 #define	NITER_DEFAULT	20
-#define	NTOTAL	134217728
+#define	NTOTAL	33554432
 #define	CONVERTDOUBLE	FALSE
-#define COMPILETIME "26 Mar 2021"
+#define COMPILETIME "28 Mar 2021"
 #define NPBVERSION "3.0 structured"
 #define CS1 "gclang"
 #define CS2 "gclang"
