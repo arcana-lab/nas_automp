feafd7dd78b7ff71395908a63357de1bfd29b040
Simone Campanoni <simo.xan@gmail.com>
2021-03-30 14:08:23 -0500

    EP: added headers for both class B and class C

--- src/brian/EP/npbparams.h
+++ src/brian/EP/npbparams.h
@@ -1,18 +1,1 @@
-/* CLASS = C */
-/*
-c  This file is generated automatically by the setparams utility.
-c  It sets the number of processors and the class of the NPB
-c  in this directory. Do not modify it by hand.
-*/
-#define	CLASS	 'C'
-#define	M	32
-#define	CONVERTDOUBLE	FALSE
-#define COMPILETIME "26 Mar 2021"
-#define NPBVERSION "3.0 structured"
-#define CS1 "gclang"
-#define CS2 "gclang"
-#define CS3 "(none)"
-#define CS4 "-I../common"
-#define CS5 "-O3 "
-#define CS6 "-lm -mcmodel=large"
-#define CS7 "randdp"
+npbparams_C.h
---
+++ src/brian/EP/npbparams_C.h
@@ -0,0 +1,18 @@
+/* CLASS = C */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	CLASS	 'C'
+#define	M	32
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "30 Mar 2021"
+#define NPBVERSION "3.0 structured"
+#define CS1 "clang"
+#define CS2 "clang"
+#define CS3 "(none)"
+#define CS4 "-I../common"
+#define CS5 "-O3 -fopenmp"
+#define CS6 "-lm -mcmodel=large -fopenmp"
+#define CS7 "randdp"
