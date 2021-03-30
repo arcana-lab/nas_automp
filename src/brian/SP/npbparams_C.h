d15be3e4efb9c660d174824f225793c215254515
Simone Campanoni <simo.xan@gmail.com>
2021-03-30 14:09:48 -0500

    SP: added headers for both class B and class C

---
+++ src/brian/SP/npbparams_C.h
@@ -0,0 +1,19 @@
+/* CLASS = C */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	PROBLEM_SIZE	162
+#define	NITER_DEFAULT	400
+#define	DT_DEFAULT	0.00067
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "30 Mar 2021"
+#define NPBVERSION "3.0 structured"
+#define CS1 "clang"
+#define CS2 "clang"
+#define CS3 "(none)"
+#define CS4 "-I../common"
+#define CS5 "-O3 -fopenmp"
+#define CS6 "-lm -mcmodel=large -fopenmp"
+#define CS7 "randdp"
