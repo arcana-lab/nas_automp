e80634b24238bb0cfdc4c154c5c300758836367d
Simone Campanoni <simo.xan@gmail.com>
2021-04-06 13:45:04 -0500

    Fixed the scripts to avoid the red zone

--- src/brian/scripts/bitcode.sh
+++ src/brian/scripts/bitcode.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+FRONTEND_FLAGS="-fno-omit-frame-pointer -ffreestanding -fno-stack-protector -fno-strict-aliasing -fno-strict-overflow -mno-red-zone -fno-common -fgnu89-inline -g -m64 -mcmodel=large"
+
 cd BT
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm bt.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} bt.c -I . -I ../common
 mv bt.ll ../../../benchmarks/BT
 
 cd ../CG
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm cg.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} cg.c -I . -I ../common
 mv cg.ll ../../../benchmarks/CG
 
 cd ../EP
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ep.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} ep.c -I . -I ../common
 mv ep.ll ../../../benchmarks/EP
 
 cd ../FT
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ft.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} ft.c -I . -I ../common
 mv ft.ll ../../../benchmarks/FT
 
 cd ../IS
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm is.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} is.c -I . -I ../common
 mv is.ll ../../../benchmarks/IS
 
 cd ../LU
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm lu.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} lu.c -I . -I ../common
 mv lu.ll ../../../benchmarks/LU
 
 cd ../MG
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm mg.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} mg.c -I . -I ../common
 mv mg.ll ../../../benchmarks/MG
 
 cd ../SP
-automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm sp.c -I . -I ../common
+automp -O1 -Xclang -disable-llvm-passes -S -emit-llvm ${FRONTEND_FLAGS} sp.c -I . -I ../common
 mv sp.ll ../../../benchmarks/SP
