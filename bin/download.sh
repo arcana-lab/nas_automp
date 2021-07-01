4a16e5b7babbb70368fcf54eee2fe30320a2a49b
Simone Campanoni <simo.xan@gmail.com>
2021-07-01 14:00:49 -0500

    Merge with master

--- bin/download.sh
+++ bin/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# download automp git
+# Download AutoMP
 git clone https://github.com/scampanoni/automp-frontend
 
-# download noelle-outlinerPass
+# Download NOELLE
 git clone --single-branch --branch OutlinerPass https://github.com/scampanoni/noelle.git noelle_linux 
-
+git clone --single-branch --branch nautilus https://github.com/scampanoni/noelle.git noelle_nautilus
