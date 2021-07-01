b1104ebd20130ca095b899434180398532299792
Simone Campanoni <simo.xan@gmail.com>
2021-07-01 14:01:56 -0500

    Merge with master

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Download AutoMP
-git clone https://github.com/scampanoni/automp-frontend
-
-# Download NOELLE
-git clone --single-branch --branch OutlinerPass https://github.com/scampanoni/noelle.git noelle_linux 
-git clone --single-branch --branch nautilus https://github.com/scampanoni/noelle.git noelle_nautilus
+rm -rf automp-frontend noelle* ;
